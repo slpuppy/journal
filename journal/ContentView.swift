57172859a99215b59b5993931e042c5d9cfecddc
gabriel <puppizin@gmail.com>
2021-08-31 08:59:37 -0300

    Initial Commit

---
+++ journal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  journal
+//
+//  Created by Gabriel Puppi on 31/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Gabriel Puppi on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
