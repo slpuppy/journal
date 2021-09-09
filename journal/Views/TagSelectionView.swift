e7fa739d182c7771f09618f671cd3aa46ea6a1fa
Naomi <brunanaomi.silva@gmail.com>
2021-09-09 09:38:22 -0300

    creating a new branch

---
+++ journal/Views/TagSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TagSelectionView.swift
+//  journal
+//
+//  Created by Bruna Naomi Yamanaka Silva on 09/09/21.
+//
+
+import SwiftUI
+
+struct TagSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagSelectionView()
+    }
+}
