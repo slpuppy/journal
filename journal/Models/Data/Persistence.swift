06dba962502bb7972fc2e505f169f5a96ccb662a
Paulo Tadashi Tokikawa <pttokikawa@live.com>
2021-09-09 09:38:40 -0300

    Create new branch Create dataModel Starting tests on dataModel

---
+++ journal/Models/Data/Persistence.swift
@@ -0,0 +1,23 @@
+//
+//  Persistence.swift
+//  journal
+//
+//  Created by Paulo Tadashi Tokikawa on 09/09/21.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "Journal")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error)")
+            }
+
+        }
+    }
+}
