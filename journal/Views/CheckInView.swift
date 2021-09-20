19526d2c177499d662e483c65e9a19c5197e4f23
Naomi <brunanaomi.silva@gmail.com>
2021-09-19 21:33:05 -0300

    back an close no checki-in

--- journal/Views/CheckInView.swift
+++ journal/Views/CheckInView.swift
@@ -138,6 +138,16 @@ struct CheckInView: View {
                             }.padding()
                         }
                     case .rating:
+                        HStack(){
+                            backButton().onTapGesture {
+                                value = value - 1
+                                viewRouter.currentPage = .question1
+                            }.padding(.horizontal, 20)
+                            Spacer()
+                            closeButton().onTapGesture {
+                                presentationMode.wrappedValue.dismiss()
+                            }.padding(.horizontal, 20)
+                        }
                         RatingView()
                     }
                     
